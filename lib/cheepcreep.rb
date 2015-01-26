0211051ccfb83c9b7f9bc6d00929a283a42ec8cf
Brit Butler <redline6561@gmail.com>
2015-01-26 13:02:41 -0500

    Scaffold out the Github class.

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -1,9 +1,21 @@
 require "cheepcreep/version"
 require "cheepcreep/init_db"
+require "httparty"
 require "pry"
 
 module Cheepcreep
 end
+
+class Github
+  include HTTParty
+  base_uri 'https://api.github.com'
+
+  def initialize
+    # ENV["FOO"] is like echo $FOO
+    #@auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+  end
+end
+
 
 class CheepcreepApp
 end

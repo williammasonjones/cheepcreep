a7342e3417d5d465a4fb66e74552c30755c02d51
William Jones <williammasonjones@gmail.com>
2015-01-26 21:52:06 -0500

    messed around w/ get_gists method

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -12,15 +12,27 @@ class Github
 
   def initialize
     # ENV["FOO"] is like echo $FOO
-    #@auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+    @auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+  end
+
+  # def get_followers(screen_name)
+  #   self.class.get("/users/followers")
+  # end
+
+  # def get_user(screen_name)
+  #   self.class.get()
+  # end
+
+  def get_gists(screen_name)
+    self.class.get("/users/#{screen_name}/gists", :headers => {"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"})
+    # json = JSON.parse(result.body)
   end
 end
 
 
-class CheepcreepApp
-end
-
 binding.pry
 
-creeper = CheepcreepApp.new
-creeper.creep
+# github = Github.new
+# resp = github.get_followers('redline6561')
+# followers = JSON.parse(resp.body)
+# Cheepcreep::GithubUser
